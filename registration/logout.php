e1393f8be661505735eb55e48f250ca5f61e5ea6
Muhammad Talha <117847229+imiantalha@users.noreply.github.com>
2026-09-09 16:00:04 +0500

    style: format registration logout PHP

--- registration/logout.php
+++ registration/logout.php
@@ -1,2 +1,26 @@
 <?php
-declare(strict_types=1);require '../includes/security.php';start_secure_session();$_SESSION=[];if(ini_get('session.use_cookies')){$params=session_get_cookie_params();setcookie(session_name(),'',time()-42000,$params['path'],$params['domain'],$params['secure'],$params['httponly']);}session_destroy();header('Location: login.php?logged_out=1');exit;
+
+declare(strict_types=1);
+
+require '../includes/security.php';
+
+start_secure_session();
+$_SESSION = [];
+
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
+session_destroy();
+header('Location: login.php?logged_out=1');
+exit;
